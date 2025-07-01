21275edb1834dfe4f9dd37c0aad5774552cd3541
NhatBangLe <bangle37568@gmail.com>
2025-07-01 16:20:02 +0700

    refactor code and add missing classes

--- src/config/model/recognizer/__init__.py
+++ src/config/model/recognizer/__init__.py
@@ -1,8 +1,8 @@
-from pydantic import Field
+from pydantic import Field, BaseModel, field_validator
 
 from src.config.model import Configuration
 
-__all__ = ["RecognizerConfiguration", "image"]
+__all__ = ["RecognizerConfiguration", "image", "ClassDescriptor", "RecognizerOutput"]
 
 
 class RecognizerConfiguration(Configuration):
@@ -15,3 +15,26 @@ class RecognizerConfiguration(Configuration):
     max_results: int = Field(description="The maximum number of results recognized is used for prompting.",
                              default=4, ge=1, le=50)
     output_config_path: str = Field(description="Path to an output configuration file.")
+
+
+class ClassDescriptor(BaseModel):
+    name: str = Field(description="Name of data class", min_length=1)
+    description: str = Field(description="Description for this class, use to search relevant information.",
+                             min_length=10)
+
+
+# noinspection PyNestedDecorators
+class RecognizerOutput(BaseModel):
+    is_configured: bool = Field(default=False, description="Whether the recognizer is configured")
+    classes: list[ClassDescriptor] = Field(description="A list of data classes")
+
+    @field_validator("classes", mode="after")
+    @classmethod
+    def remove_classes_duplicate(cls, classes: list[ClassDescriptor]):
+        class_names = set()
+        nodup_classes: list[ClassDescriptor] = []
+        for data_class in classes:
+            if data_class.name not in class_names:
+                class_names.add(data_class.name)
+                nodup_classes.append(data_class)
+        return nodup_classes
