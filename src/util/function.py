c6d7d72eb640a48373f1cf9da020b58cb60a94b3
NhatBangLe <bangle37568@gmail.com>
2025-07-19 09:49:48 +0700

    refactor code

--- src/util/function.py
+++ src/util/function.py
@@ -1,34 +1,9 @@
-import datetime
 import os
 import uuid
 import zipfile
 from pathlib import Path
 
-from src.util.constant import DEFAULT_TIMEZONE
-from src.util.error import InvalidArgumentError
-
-
-def convert_datetime_to_str(datetime_obj: datetime.datetime) -> str:
-    """
-    Convert a datetime object to string.
-    `DEFAULT_TIMEZONE` is used as the timezone.
-    """
-    return datetime_obj.astimezone(DEFAULT_TIMEZONE).isoformat()
-
-
-def convert_str_to_datetime(datetime_str: str) -> datetime.datetime:
-    """
-    Convert a string to a datetime object.
-    The `datetime_str` must be in ISO 8601 format.
-    `DEFAULT_TIMEZONE` is used as the timezone.
-
-    Args:
-        datetime_str: String representation of a datetime object
-
-    Raises:
-        ValueError: If datetime string is invalid
-    """
-    return datetime.datetime.fromisoformat(datetime_str).astimezone(DEFAULT_TIMEZONE)
+from .error import InvalidArgumentError
 
 
 def strict_uuid_parser(uuid_string: str) -> uuid.UUID:
@@ -61,3 +36,7 @@ def zip_folder(folder_path: str | os.PathLike[str], output_path: str | os.PathLi
         for file_path in folder.rglob('*'):
             if file_path.is_file():
                 zipf.write(file_path, file_path.relative_to(folder))
+
+
+def get_cache_dir_path():
+    return os.getenv("CACHE_DIR", "/app/cache")
